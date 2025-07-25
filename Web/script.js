d738ac83128b67addb6436ad9db86da8184df2ca
angehgg <angelgg2912@gmail.com>
2025-07-25 22:40:31 +0200

    html js y recursos v1

---
+++ Web/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    /*const pokedexSection = document.getElementById('pokedexSection');
+    const pokeSearch = document.getElementById('pokeForm');
+    const sections = document.getElementById('sections');
+
+    if (pokedexSection && pokeSearch) {
+        pokedexSection.addEventListener('click', function() {
+            pokeSearch.classList.toggle('collapsed');
+            sections.classList.toggle('collapsed');
+            console.log(sections.className);
+    });*/
+
+    
+});
