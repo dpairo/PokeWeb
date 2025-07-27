eda4a98e2d0cf232800ef8b8a2bb8f1092afe8f7
angehgg <angelgg2912@gmail.com>
2025-07-27 18:45:34 +0200

    lista de pokemons, buscador, iconos de tipos

--- Web/script.js
+++ Web/script.js
@@ -1,14 +1,83 @@
-document.addEventListener("DOMContentLoaded", function() {
-    /*const pokedexSection = document.getElementById('pokedexSection');
-    const pokeSearch = document.getElementById('pokeForm');
-    const sections = document.getElementById('sections');
+document.addEventListener("DOMContentLoaded", function () {
+    const pokemonList = document.getElementById('pokemonList');
+    const pokeSearch = document.getElementById('pokeSearch')
+    const searchBtn = document.getElementById('search-button')
 
-    if (pokedexSection && pokeSearch) {
-        pokedexSection.addEventListener('click', function() {
-            pokeSearch.classList.toggle('collapsed');
-            sections.classList.toggle('collapsed');
-            console.log(sections.className);
-    });*/
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=10z&offset=0";
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Respuesta no válida del servidor");
+            }
+            return response.json();
+        })
+        .then(data => {
+            data.results.forEach(pokemon => {
+                const id = pokemon.url.split("/").filter(Boolean).pop();
+                createPokemon(id);
 
-    
+            });
+            /*console.log(data); // Información completa del resultado*/
+        })
+        .catch(error => {
+            console.error("Error cargando Pokémon:", error);
+        });
+
+        searchBtn.addEventListener('click',async function() {
+            console.log(pokeSearch.value);
+            if(pokeSearch.value.trim() !== ''){
+                try {
+                    pokemonList.innerHTML = "";
+                    createPokemon(pokeSearch.value);
+
+                } catch (error) {
+                    console.log("pokemon desconocido",error)
+                }
+            }
+        });
+
+        async function createPokemon(pokemon){
+                try {
+                    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+                    if (!res.ok) throw new Error("No se ha encontrado el Pokémon");
+                    
+                    const data = await res.json();
+                    console.log(data);
+
+                    const poke = document.createElement('a');
+                    poke.href = "#";
+                    poke.classList.add('preview');
+
+                    const img = document.createElement('img');
+                    img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`;
+                    img.alt = data.name;
+
+                    const pokeName = document.createElement('p');
+                    pokeName.textContent = `${data.name} #${data.id}`;
+
+                    poke.appendChild(img);
+                    poke.appendChild(pokeName);
+
+                    data.types.forEach(t => {
+                        const typeName = t.type.name; // ejemplo: 'fire', 'water', etc.
+
+                        const typeContainer = document.createElement('div');
+                        typeContainer.classList.add('icon', typeName); // útil si tienes estilos CSS por tipo
+
+                        const typeIcon = document.createElement('img');
+                        typeIcon.src = `../resources/TypeIcons/icons/${typeName}.svg`;
+                        typeIcon.alt = typeName;
+
+                        typeContainer.appendChild(typeIcon);
+                        poke.appendChild(typeContainer);
+                    });
+
+                    pokemonList.appendChild(poke);
+
+                } catch (error) {
+                    console.error(error.message);
+                    alert("Error al buscar el Pokémon. Intenta con otro nombre o ID.");
+                }
+
+        }
 });
