095ac3939fd574b1697b2a97fb4bc95cf3e52d55
Dpairoo <44551790+dpairo@users.noreply.github.com>
2025-08-03 19:40:22 +0100

    Create Backend and Repositories splitting

---
+++ Backend/src/domain/pokemon.rs
@@ -0,0 +1,27 @@
+#[derive(Debug, Clone)]
+struct Ability {
+    name: String,
+    is_hidden: bool
+}
+
+#[derive(Debug, Clone)]
+struct Statistics {
+    pub hp: u8,
+    pub attack : u8,
+    pub defense: u8,
+    pub special_attack: u8,
+    pub special_defense: u8,
+    pub speed : u8
+}
+#[derive(Debug, Clone)]
+pub struct Pokemon {
+    id: u16,
+    name: String,
+    stats: Statistics,
+    hatch_counter: u8,
+    base_experience: u16,
+    egg_groups: Vec<String>,
+    typing: Vec<String>,
+    evolution_chain: Vec<String>,
+    abilities: Vec<Ability>
+}
