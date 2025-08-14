427e16d8ae7413739e897715703fb67a8da7ee72
Dpairoo <44551790+dpairo@users.noreply.github.com>
2025-08-14 16:33:31 +0100

    triynig git stash (it's amazing)

---
+++ Backend/src/infrastructure/DTO/pokemonDTO.rs
@@ -0,0 +1,58 @@
+use serde::Deserialize;
+use crate::infrastructure::DTO::shared::named_api_resource;
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct PokemonDTO {
+    pub id: u16,
+    pub name: String,
+    pub base_experience: Option<u16>,
+    pub height: Option<u16>,
+    pub weight: Option<u16>,
+    pub species: NamedAPIResource,
+
+    #[serde(default)]
+    pub stats: Vec<PokemonStatDTO>,
+
+    #[serde(default)]
+    pub types: Vec<PokemonTypeDTO>,
+
+    #[serde(default)]
+    pub moves: Vec<PokemonMoveDTO>,
+
+    #[serde(default)]
+    pub abilities: Vec<PokemonAbilityDTO>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct PokemonStatDTO {
+    pub base_stat: u16,
+    pub effort: u8,
+    pub stat: NamedAPIResource,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct PokemonTypeDTO {
+    pub slot: u8,
+    pub r#type: NamedAPIResource,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct PokemonAbilityDTO {
+    pub ability: NamedAPIResource,
+    pub is_hidden: bool,
+    pub slot: u8,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct PokemonMoveDTO {
+    pub r#move: NamedAPIResource,
+    #[serde(default)]
+    pub version_group_details: Vec<MoveVersionDTO>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct MoveVersionDTO {
+    pub level_learned_at: u8,
+    pub move_learn_method: NamedAPIResource,
+    pub version_group: NamedAPIResource,
+}
---
+++ Backend/src/infrastructure/DTO/shared/named_api_resource.rs
@@ -0,0 +1,34 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, Clone, Eq, PartialEq, Hash)]
+pub struct NamedAPIResource {
+    pub name: String,
+    pub url: String,
+}
+
+#[derive(Deserialize, Debug, Clone, Eq, PartialEq, Hash)]
+pub struct EvolutionAPIResource {
+    pub url: String,
+}
+
+impl EvolutionAPIResource {
+    pub fn id(&self) -> Option<u32> { id_from_url(&self.url) }
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct EvolutionChainDTO {
+    pub chain: ChainLink,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct ChainLink {
+    pub species: NamedAPIResource,
+    #[serde(default)]
+    pub evolves_to: Vec<ChainLink>,
+}
+pub fn id_from_url(url: &str) -> Option<u32> {
+    url.trim_end_matches('/')
+        .rsplit('/')
+        .next()
+        .and_then(|s| s.parse::<u32>().ok())
+}
